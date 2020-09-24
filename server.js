ea191e2935b2f6cae5a2b9cd517820dc5dd36d53
Your Name <you@example.com>
2020-09-24 23:30:40 +0530

    Heroku commit -1.1

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ mongoose
   .then((con) => {
     console.log("DB connection Successful");
   });
-
-const port = process.env.PORT;
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 5000
+}
 app.listen(port, () => {
   console.log(`Server Started on Port ${port}...`);
 });
